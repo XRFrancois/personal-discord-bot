faa46bebd1419dc52e9536a3f3c58134c34f4269
xavier.francois.001@student.uni.lu <xavier.francois@hotmail.com>
2025-03-04 23:20:44 +0100

    Added separate file for storage of the emoji-role pairs and cleaned up the code a bit.

--- src/main.py
+++ src/main.py
@@ -6,6 +6,10 @@ from discord import app_commands
 import logging
 import csv
 
+
+
+""" CONFIG & SETUP """
+
 # Keys are stored locally in the '.env' file for security reasons.
 load_dotenv()
 
@@ -19,13 +23,8 @@ GUILD = discord.Object(id=os.getenv("GUILD_ID"))
 # Sets the ID of the message. TODO extract that and make it more configurable
 selector_message_id = 1346498705976594512
 
-# Creates a list of emojis - roles pairs. Must return a role for a given emoji.
-emojis_roles = {
-    '🏴‍☠️': 886560442305089547, 
-    '👍🏽': 688055636629389315
-}
-
-# TODO make a separate lookup table of the pairs to pick the correct ones. From 
+# TODO add an easier way to add entries to the text file (convert emojis) >> Can take from https://emojipedia.org/video-game
+# TODO export all the roles from the server and give them a random emoji?
 # Retrieves the pairs of emojis - roles from the "database" data/emojis-roles.txt
 emojis_roles = {}
 with open('data/emojis_roles.txt', mode ='r', encoding='utf-8') as file:
@@ -33,6 +32,7 @@ with open('data/emojis_roles.txt', mode ='r', encoding='utf-8') as file:
         line = line.replace("\n", "")
         values = line.split(',')
         emojis_roles[values[0]] = int(values[1])
+        print(">> Loaded: ", values[0], values[1], values[2])
     file.close()
 
 # Loads the intents for the bot
@@ -47,7 +47,11 @@ tree = app_commands.CommandTree(client)
 # Creates a handler for the logs
 # handler = logging.FileHandler(filename='logs/discord.log', encoding='utf-8', mode='w')
 
-# This event is called whenever the bot sends a callback function notifiying us that it is ready.
+
+
+""" EVENTS TRIGGER """
+
+# Whenever the bot sends a callback function notifiying us that it is ready.
 @client.event
 async def on_ready():
     print(f'BOT logged in as: {client.user}')
@@ -59,6 +63,7 @@ async def on_ready():
         print(e)
 
     # TODO if there is no role selection message posted and linked, creates one and links it. RSM should be fetched from .env variables
+
 
 # Whenever there is a message posted in the server on which the bot is installed
 @client.event
@@ -81,11 +86,13 @@ async def on_message(message):
         # Synchronizes the commands into the tree
         await tree.sync(guild=GUILD)
 
+
 # Whenever someone edits a message
 @client.event
 async def on_message_edit(before, after):
     msg = f'**{before.author}** edited their message:\n{before.content} -> {after.content}'
     print(msg)
+
 
 # Whenever someone deletes a message
 @client.event
@@ -93,26 +100,88 @@ async def on_message_delete(message):
     msg = f'{message.author} has deleted the message: {message.content}'
     print(msg)
 
+
 # Whenever someone joins the server
 @client.event
 async def on_member_join(member):
     guild = member.guild
 
-    # TODO add a way to specify the channel.
+    # TODO add a way to specify the channel. Can I change the specific channel?
 
     # Sends a message in the system channel
     if guild.system_channel is not None:
         to_send = f'Welcome {member.mention} to {guild.name}!'
         await guild.system_channel.send(to_send)
 
-    # Can I change the specific channel?
 
-# Creates a bot that will respond to commands with the / prefix
+# Whenever someone reacts to the specific message, gives roles according to the emoji (OR commands?)
+@client.event
+async def on_raw_reaction_add(payload):
+
+    # Ignores if the id of the message is not the desired one
+    if payload.message_id != selector_message_id:
+        return
+
+    # Extracts the roles from the server
+    guild = client.get_guild(payload.guild_id)
+    channel = client.get_channel(payload.channel_id)
+    
+    # Retrieves the name & ID from the reaction
+    reaction_emoji_name, reaction_emoji_name_id = payload.emoji.name, payload.emoji.id
+    print(">> Reaction Emoji:", reaction_emoji_name)
+
+    # Checks if the reaction emoji is in the list.
+    if reaction_emoji_name not in emojis_roles.keys():
+        return
+
+    # Gets the role ID linked to the emoji name from the table
+    role_to_add = guild.get_role(emojis_roles[reaction_emoji_name])
+
+    # Adds a specific role based on the reaction
+    reacting_user = payload.member
+    print(">> Added role", role_to_add,"to user", reacting_user)
+    await reacting_user.add_roles(role_to_add)
+
+
+# Whenever someone reacts to the specific message, gives roles according to the emoji (OR commands?)
+@client.event
+async def on_raw_reaction_remove(payload):
+
+    # Ignores if the id of the message is not the desired one
+    if payload.message_id != selector_message_id:
+        return
+
+    # Extracts the roles from the server
+    guild = client.get_guild(payload.guild_id)
+    channel = client.get_channel(payload.channel_id)
+    
+    # Retrieves the name & ID from the reaction
+    reaction_emoji_name, reaction_emoji_name_id = payload.emoji.name, payload.emoji.id
+    print(">> Reaction to removed Emoji:", reaction_emoji_name)
+
+    # Checks if the reaction emoji is in the list.
+    if reaction_emoji_name not in emojis_roles.keys():
+        return
+
+    # Gets the role ID linked to the emoji name from the table
+    role_to_remove = guild.get_role(emojis_roles[reaction_emoji_name])
+
+    # Removes a specific role based on the emoji reaction. Fetch is used because get_member() would lead to cache misses that crashed the program.
+    reacting_user = await guild.fetch_member(payload.user_id)
+    print(">> Removed role", role_to_remove,"from user", reacting_user)
+    await reacting_user.remove_roles(role_to_remove)
+
+
+
+""" BOT COMMANDS """
+
+# TODO rework or remove. Creates a bot that will respond to commands with the / prefix
 @tree.command(name='test', description="A command for xav's bot", guild=GUILD)
 async def test(interaction: discord.Interaction):
     await interaction.response.send_message("test successful")
 
-# Creates a bot that will respond to commands with the / prefix
+
+# TODO rework or remove. Creates a bot that will respond to commands with the / prefix
 @tree.command(name='parrot', description="The bot will repeat what it hears the number of times specified.", guild=GUILD)
 # Describes the parameters that are passed to the slash command
 @app_commands.describe(input_text="The text that the bot will repeat.", repeats_count="How many times the bot will repeat the text.")
@@ -122,7 +191,8 @@ async def parrot(interaction: discord.Interaction, input_text: str, repeats_coun
         return_text += input_text + "\n"
     await interaction.response.send_message(return_text)
 
-# Synchronizes the slash commands
+
+# Command to synchronizes the slash commands from the code
 @tree.command(name='sync_commands', description="Syncs the commands onto the server. May require to reload the page.", guild=GUILD)
 @app_commands.describe(input_password="Enter the password to validate the action.")
 async def sync_commands(interaction: discord.Interaction, input_password: str):
@@ -134,80 +204,16 @@ async def sync_commands(interaction: discord.Interaction, input_password: str):
     else:
         await interaction.response.send_message("Incorrect password.")
 
-# A command to publish leetcodes onto the server ?
+
+# TODO rework or remove. A command to publish leetcodes onto the server ?
 @tree.command(name='leetcode_report', description="This will report the latest leetcodes completed.", guild=GUILD)
 @app_commands.describe(username="The target user.")
 async def leetcode_report(interaction: discord.Interaction, username: str):
 
     await interaction.response.send_message(f"User:{username}")
 
-# Whenever someone reacts to the specific message, gives roles according to the emoji (OR commands?)
-@client.event
-async def on_raw_reaction_add(payload):
 
-
-    # Ignores if the id of the message is not the desired one
-    if payload.message_id != selector_message_id:
-        return
-
-    # Extracts the roles from the server
-    guild = client.get_guild(payload.guild_id)
-    channel = client.get_channel(payload.channel_id)
-    
-    # Retrieves the name & ID from the reaction
-    reaction_emoji_name, reaction_emoji_name_id = payload.emoji.name, payload.emoji.id
-    print("emoji", reaction_emoji_name, reaction_emoji_name_id)
-
-    # Checks if the reaction emoji is in the list. TODO add a check to avoid crashes when it is not in the list and also return aerly
-    if reaction_emoji_name not in emojis_roles.keys():
-        
-        print(reaction_emoji_name, "not found in table.")
-        return
-
-    # Gets the role ID linked to the emoji name from the table
-    role_to_add = guild.get_role(emojis_roles[reaction_emoji_name])
-    print("role ID to add", role_to_add)
-
-    # Adds a specific role based on the reaction
-    reacting_user = payload.member
-    print("Adding role", role_to_add,"to user", reacting_user)
-    await reacting_user.add_roles(role_to_add)
-
-
-# Whenever someone reacts to the specific message, gives roles according to the emoji (OR commands?)
-@client.event
-async def on_raw_reaction_remove(payload):
-
-    print(payload)
-
-    # Ignores if the id of the message is not the desired one
-    if payload.message_id != selector_message_id:
-        return
-
-    # Extracts the roles from the server
-    guild = client.get_guild(payload.guild_id)
-    channel = client.get_channel(payload.channel_id)
-    
-    # Retrieves the name & ID from the reaction
-    reaction_emoji_name, reaction_emoji_name_id = payload.emoji.name, payload.emoji.id
-    print("emoji", reaction_emoji_name, reaction_emoji_name_id)
-
-    # Checks if the reaction emoji is in the list. TODO add a check to avoid crashes when it is not in the list and also return aerly
-    if reaction_emoji_name not in emojis_roles.keys():
-        
-        print(reaction_emoji_name, "not found in table.")
-        return
-
-    # Gets the role ID linked to the emoji name from the table
-    role_to_remove = guild.get_role(emojis_roles[reaction_emoji_name])
-    print("role ID to remove", role_to_remove) # Why is this returing a string and not the id?
-
-    # removes a specific role based on the reaction
-    reacting_user = guild.get_member(payload.user_id)
-    print("Removing role", role_to_remove,"to user", reacting_user)
-    await reacting_user.remove_roles(role_to_remove)
-
-# A command add a new emoji-role pair to the "database" file.
+# Command to add a new emoji-role pair to the "database" file.
 @tree.command(name='add_emoji_role_pair', description="This allows users to add a new emoji-role relation into the database. Requires the password.", guild=GUILD)
 @app_commands.describe(input_password="Password to validate the request.", input_emoji="The emoji that will be associated to the role.", input_role="The role associated to the emoji.")
 async def add_emoji_role_pair(interaction: discord.Interaction, input_password: str, input_emoji: str, input_role: str):
@@ -217,19 +223,17 @@ async def add_emoji_role_pair(interaction: discord.Interaction, input_password: 
         
         # Attempts to find the role by ID on the guild.
         try:
-            role_name = interaction.guild.get_role(int(input_role))
-            print(role_name)
+            role_name = interaction.guild.get_role(int(input_role)).name
         except:
             print("Provided ID did not match an existing role.")
             await interaction.response.send_message("Provided ID did not match an existing role.")
 
         # Adds the pair to the database file.
         with open('data/emojis_roles.txt', mode ='a', encoding='utf-8') as file:
-            line = "\n" + input_emoji + "," + input_role
-            print(line)
+            line = "\n" + input_emoji + "," + input_role + "," + role_name
             file.write(line)
-            file.close()
-            await interaction.response.send_message(f"### Command result:\nSuccessfully added the pair **Emoji**: {input_emoji} - **Role**: *{role_name}* to the database.")
+        file.close()
+        await interaction.response.send_message(f"## Command result:\nSuccessfully added the pair **Emoji**: {input_emoji} - **Role**: {role_name} to the database.")
     else:
         await interaction.response.send_message("Incorrect password.")
 
